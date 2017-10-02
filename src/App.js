b2d10cf0df593fbdaf4c1c0bd685e3c61ba68a71
JesseSoldat <jsoldat@hotmail.com>
2017-10-02 10:21:20 -0400

    search bar works

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import {debounce} from 'lodash';
+const API_KEY = 'AIzaSyAqATbr65c9EON9JW6nSf5yEiHK-mtpdPg';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch("anime");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term }, videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+      console.log(this.state);
+    });
+  }
+
   render() {
+    const videoSearch = debounce(term => {
+      this.videoSearch(term);
+    }, 300);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
       </div>
     );
   }
